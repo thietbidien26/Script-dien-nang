adc2edb3dc566099f00576b85705045be13c0bfb
thietbidien26 <138670566+thietbidien26@users.noreply.github.com>
2023-07-05 17:05:29 +0700

    Add files via upload

---
+++ headerscript.js
@@ -0,0 +1,13 @@
+  <script>
+    document.addEventListener("DOMContentLoaded", function() {
+      var linkElements = document.getElementsByTagName("link");
+      for (var i = 0; i < linkElements.length; i++) {
+        var linkElement = linkElements[i];
+        var relAttribute = linkElement.getAttribute("rel");
+        var hrefAttribute = linkElement.getAttribute("href");
+        if (relAttribute === "canonical" && (hrefAttribute === "https://diennangdongnai.com:443/" || hrefAttribute === "https://diennangdongnai.com")) {
+          linkElement.parentNode.removeChild(linkElement);
+        }
+      }
+    });
+  </script>
